c9e5c6110b7672ae8cce87f7a575966245459fbd
LowellC86 <lowellcaraballo@gmail.com>
2023-05-22 10:16:44 -0400

    Hip Hop HangMan Start

---
+++ hip_hop_hangman/script.js
@@ -0,0 +1,13 @@
+//psuedo code steps: 
+
+//1. create word list within an array
+//2. create function to log keystrokes 
+//3. check if letter is already pressed and display guess
+//4. compare keystrokes to the word [true/false] 
+//5. change the number of guesses left
+//6. update dom correct guess or incorrect guess)
+//7. check if they have any guesses left 
+//if no  - ‘game over’
+//8. check if they won - start over
+
+
